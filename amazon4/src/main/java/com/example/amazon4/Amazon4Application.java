4a8c77602b29b0ee3f3dad77eac1ca3a5c79e715
ground <groundsnail@163.com>
2023-02-18 21:32:25 +0800

    initial

---
+++ amazon4/src/main/java/com/example/amazon4/Amazon4Application.java
@@ -0,0 +1,13 @@
+package com.example.amazon4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Amazon4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Amazon4Application.class, args);
+    }
+
+}
